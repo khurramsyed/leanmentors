11349f5e622fd6eea3f01969492d5a909720a05b
Khurram Mahmood <khurram@khurrams-mbp.mynet>
2017-02-19 16:29:15 +0000

    Adding Client App

---
+++ cloud-config-client/src/main/java/com/leanementors/SampleController.java
@@ -0,0 +1,25 @@
+package com.leanementors;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by khurram on 19/02/2017.
+ */
+
+@Controller
+public class SampleController {
+
+    @Value("${application.name}")
+     String luckyWord;
+
+    @RequestMapping("/lucky-word")
+    public String showLuckyWord(){
+        System.out.print("Lucky words is "+ luckyWord);
+
+        return "Hello :"+luckyWord;
+    }
+
+
+}
