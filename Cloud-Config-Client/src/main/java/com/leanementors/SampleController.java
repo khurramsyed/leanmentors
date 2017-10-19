17eee1ab651014757e097284794441c1d98e2304
Khurram Mahmood <mahmood.khurram@gmail.com>
2017-10-19 08:45:30 +0100

    Made some changes

--- Cloud-Config-Client/src/main/java/com/leanementors/SampleController.java
+++ Cloud-Config-Client/src/main/java/com/leanementors/SampleController.java
@@ -3,12 +3,13 @@ package com.leanementors;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by khurram on 19/02/2017.
  */
 
-@Controller
+@RestController
 public class SampleController {
 
     @Value("${name}") String luckyWord;
